46e43674fabf762c1d0150dbb8e227d3e12b8646
Ana Zimerer <ana.zimerer@gmail.com>
2020-07-06 19:08:05 -0300

    Rotas

---
+++ src/Pages/FeedPage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FeedPage(){
+    return(
+        <div>
+            FeedPage
+        </div>
+    );
+}
+
---
+++ src/Routes/routes.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Switch, Route , BrowserRouter } from 'react-router-dom'
+
+import Login from '../Pages/LoginPage/index.js';
+import SigIn from '../Pages/SigInPage/index.js'
+import Feed from '../Pages/FeedPage/index.js'
+import Post from '../Pages/PostPage/index.js'
+
+export default function Routes() {   
+    return (
+        <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Login}/> 
+              <Route exact path="/sigin" component={SigIn}/>  
+              <Route exact path="/feed" component={Feed}/>   
+              <Route exact path="/posts" component={Post}/>                
+            </Switch>
+        </BrowserRouter>
+    );
+  }
+  
